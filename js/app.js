9cb612b1001cae821643bc29f0a52fdc13c3f8bc
Manoj <89052931+pathwaymk@users.noreply.github.com>
2022-09-27 07:23:45 +0530

    Add resume in open cv button

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-const button = document.querySelector(".btn1")
-const button2 = document.querySelector(".btn2")
-const silde2 = document.querySelector(".slide2")
-const slide1 = document.querySelector(".slide1")
+// const button = document.querySelector(".btn1")
+// const button2 = document.querySelector(".btn2")
+// const silde2 = document.querySelector(".slide2")
+// const slide1 = document.querySelector(".slide1")
 const nav = document.querySelector("nav")
 const navcontent = document.querySelector(".sideMenu")
+const cvBtn = document.querySelector("#cvbtn")
+const gettouch = document.querySelector(".touch")
 
-button.addEventListener("click", ()=>{
-    slide1.classList.remove("selected")
-    silde2.classList.remove("selected")
-    button.classList.add("selectedbtn")
-    button2.classList.remove("selectedbtn")
-})
-button2.addEventListener("click", ()=>{
-    silde2.classList.add("selected")
-    slide1.classList.add("selected")
-    button.classList.remove("selectedbtn")
-    button2.classList.add("selectedbtn")
-})
+// button.addEventListener("click", ()=>{
+//     slide1.classList.remove("selected")
+//     silde2.classList.remove("selected")
+//     button.classList.add("selectedbtn")
+//     button2.classList.remove("selectedbtn")
+// })
+// button2.addEventListener("click", ()=>{
+//     silde2.classList.add("selected")
+//     slide1.classList.add("selected")
+//     button.classList.remove("selectedbtn")
+//     button2.classList.add("selectedbtn")
+// })
 
 nav.addEventListener("click", ()=>{
     nav.classList.toggle("clicked")
     navcontent.classList.toggle("show")
+})
+cvBtn.addEventListener("click", ()=>{
+    let resume = window.open("D:/Material/portfolio website/Resume2.pdf")
+    resume.document.title = "Resume"
+})
+gettouch.addEventListener("click", ()=>{
+    location.href = "mailto:manojoff21@gmail.com"
 })
